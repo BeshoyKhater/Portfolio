bb5cd8c2e0bbe69bcf17eb4382504c9dac93ded6
BeshoyKhater <BeshoyKhater@gmail.com>
2023-02-09 20:15:06 +0200

    Edit Nav

--- src/sections/FloatingNav.jsx
+++ src/sections/FloatingNav.jsx
@@ -31,14 +31,16 @@ const FloatingNav = () => {
 
   return (
     <section id="floating__nav">
-      <div className="container floating__container">
-        <article>
-          <a data-name="Header" href="#Header"><AiTwotoneHome/></a>
-          <a data-name="About" href="#About"><BsFillPersonFill/></a>
-          <a data-name="Services" href="#Services"><MdMedicalServices/></a>
-          <a data-name="Portfolio" href="#Portfolio"><HiSquares2X2/></a>
-          <a data-name="Contact" href="#Contact"><AiFillMessage/></a>
-        </article>
+      <div className="container ">
+        <div className="floating__container">
+          <article>
+            <a data-name="Header" href="#Header"><AiTwotoneHome/></a>
+            <a data-name="About" href="#About"><BsFillPersonFill/></a>
+            <a data-name="Services" href="#Services"><MdMedicalServices/></a>
+            <a data-name="Portfolio" href="#Portfolio"><HiSquares2X2/></a>
+            <a data-name="Contact" href="#Contact"><AiFillMessage/></a>
+          </article>
+        </div>
       </div>
     </section>
   )
